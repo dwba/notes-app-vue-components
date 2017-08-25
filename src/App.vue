be29b712ccfe8e45be80806eba30f3d004057333
Peter Jang <peter.x.jang@gmail.com>
2017-08-25 13:43:08 -0500

    Add search input functionality to dynamically filter notes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-    <toolbar v-on:clickNew="createNote" v-on:clickDelete="deleteNote"></toolbar>
+    <toolbar
+      v-on:clickNew="createNote"
+      v-on:clickDelete="deleteNote"
+      v-bind:searchNoteText="searchNoteText"
+      v-on:inputSearchNoteText="updateSearch"
+    >
+    </toolbar>
     <note-container
       v-bind:notes="notes"
       v-bind:transformedNotes="transformedNotes"
@@ -26,7 +32,8 @@ export default {
     ];
     return {
       notes: initialNotes,
-      selectedNote: initialNotes[0]
+      selectedNote: initialNotes[0],
+      searchNoteText: ""
     };
   },
   methods: {
@@ -56,13 +63,25 @@ export default {
           this.selectedNote = {};
         }
       }
+    },
+    updateSearch: function(newSearchText) {
+      this.searchNoteText = newSearchText;
+      if (this.transformedNotes.length === 0) {
+        this.selectedNote = {};
+      } else if (this.transformedNotes.indexOf(this.selectedNote) === -1) {
+        this.selectedNote = this.transformedNotes[0];
+      }
     }
   },
   computed: {
     transformedNotes: function() {
-      return this.notes.slice().sort(function(a, b) {
-        return b.timestamp - a.timestamp;
-      });
+      return this.notes
+        .filter(function(note) {
+          return note.body.toLowerCase().indexOf(this.searchNoteText.toLowerCase()) !== -1;
+        }.bind(this))
+        .sort(function(a, b) {
+          return b.timestamp - a.timestamp;
+        });
     }
   },
   components: {
